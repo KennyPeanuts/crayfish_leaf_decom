4e727b6faa6a7f943119038ecae199abc2ce01b0
Fortino <fortinok@lu06296apple.longwood.edu>
2016-07-06 10:24:21 -0400

    tank ysi analysis

---
+++ lab_notebook/analysis/tank_chl_analysis.R
@@ -0,0 +1,29 @@
+# Analysis of the tank chl
+
+## Metadata
+
+* File created on 6 July 2016
+
+## Description
+
+This is the code to analyze the effect of crayfish community on the tank chlorophyll the experiment evaluating the impact of in invasive and native crayfish.
+
+## R Code
+
+### Import data
+
+    ysi <- read.table("./data/tank_YSI.csv", header = T, sep = ",")
+    chl <- read.table("./data/tank_chl.csv", header = T, sep = ",")
+    treat <- read.table("./data/tank_map.csv", header = T, sep = ",")
+
+### Merge tank map with chem data
+
+    chl <- merge(chl, treat, by = "tank")
+
+### Analyze pH by treatment
+
+    plot(Chl ~ treatment, data = chl)
+    plot(Chl ~ treatment, data = chl, subset = Date == "2016-06-03")
+    plot(Chl ~ treatment, data = chl, subset = Date == "2016-06-21")
+    
+    anova(lm(Chl ~ treatment, data = chl))
